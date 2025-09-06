648a7e8248dfb7b8563640b692b66ea26e6bbf5b
ihor-sokoliuk <yaaappee@gmail.com>
2025-09-06 15:56:13 -0500

    0.6.2

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { createHttpServer } from "./http-server.js";
 import { validateEnvironment as validateEnv } from "./error-handler.js";
 
 // Use a static version string that will be updated by the version script
-const packageVersion = "0.6.1";
+const packageVersion = "0.6.2";
 
 // Export the version for use in other modules
 export { packageVersion };
